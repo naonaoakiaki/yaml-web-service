192f2e212da2aaf997c49d6ee7f4476a6720e85b
Naoaki Hashimoto <hasimoto0130gt@gmail.com>
2017-05-27 13:11:55 +0900

    initial commit

---
+++ src/main/java/com/example/yamlwebservice/YamlWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.yamlwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamlWebServiceApplication.class, args);
+	}
+}
