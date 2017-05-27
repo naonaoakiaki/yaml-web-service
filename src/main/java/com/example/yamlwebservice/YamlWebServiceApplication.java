e0a64c841aee9750aca6cce7bc3d2345f2ab3956
Naoaki Hashimoto <hasimoto0130gt@gmail.com>
2017-05-27 13:53:39 +0900

    基本情報の表示

--- src/main/java/com/example/yamlwebservice/YamlWebServiceApplication.java
+++ src/main/java/com/example/yamlwebservice/YamlWebServiceApplication.java
@@ -1,12 +1,36 @@
 package com.example.yamlwebservice;
 
+import com.example.yamlwebservice.entity.Human;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@Controller
 public class YamlWebServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YamlWebServiceApplication.class, args);
 	}
+
+	private Human human;
+
+	@RequestMapping("/makeyaml")
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView();
+        mav.addObject("name", "taro");
+        mav.addObject("age", "16");
+        mav.addObject("sex", "men");
+		mav.setViewName("makeyaml"); //view名の指定
+		return mav;
+	}
+
+//	@RequestMapping(value = "/makeyaml")
+//	public String index(@ModelAttribute Human human) {
+//		this.human = human;
+//		return human.getName();
+//	}
 }
---
+++ src/main/java/com/example/yamlwebservice/entity/Human.java
@@ -0,0 +1,13 @@
+package com.example.yamlwebservice.entity;
+
+import lombok.Data;
+
+/**
+ * Created by naonaoakiaki on 2017/05/27.
+ */
+@Data
+public class Human {
+    private String name;
+    private int age;
+    private String sex;
+}
